73e4d7c9a8f431f691811636b2460acdf49b1189
장동호 <david@piolink.com>
2021-04-10 00:28:31 +0900

    config updated

--- www/src/config.js
+++ www/src/config.js
@@ -2,11 +2,11 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "2hlo1m87r533tgf32bi9uskrv0",     // CognitoClientID
-  "api_base_url": "https://8zu1jf4yse.execute-api.ap-northeast-2.amazonaws.com/prod",                                     // TodoFunctionApi
-  "cognito_hosted_domain": "mytodoappdemo-refarch.auth.ap-northeast-2.amazoncognito.com",                   // CognitoDomainName
-  "redirect_url": "https://master.d357a9s84vny26.amplifyapp.com"                                      // AmplifyURL
-  //"redirect_url": "http://127.0.0.1:8080"                                      // AmplifyURL
+  "aws_user_pools_web_client_id": "3qn4um721sssmnahghd0t47g3i",     // CognitoClientID
+  "api_base_url": "https://gocn4wkd79.execute-api.ap-northeast-2.amazonaws.com/prod",    // TodoFunctionApi
+  "cognito_hosted_domain": "mytodoappdemo-refarch.auth.ap-northeast-2.amazoncognito.com",  // CognitoDomainName
+  "redirect_url": "https://master.d2nq74g7a5x51y.amplifyapp.com"         // AmplifyURL
+  //"redirect_url": "http://127.0.0.1:8080"                              // AmplifyURL
 };
 
 export default config;
